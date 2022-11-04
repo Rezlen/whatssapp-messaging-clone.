0d25efaf82ceec7607182b37a04e365a4b277bc1
Rezlen <soheilyreza@yahoo.com>
2022-11-04 09:43:12 +0000

    Comletion commit

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(errorHandler);
 // app.get('/api/chat', (req, res) => {
 //   res.send(chats);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
   const server = app.listen(
     PORT,
